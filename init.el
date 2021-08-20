883162a3c2e9a0eb56919f24e18b6ffcc5c65572
Jack Faller <jack.t.faller@gmail.com>
2021-08-20 10:57:51 +0100

    added my changes to company box for speed up

--- init.el
+++ init.el
@@ -488,56 +488,48 @@
 	(unless all-the-icons-fonts-installed?
 		(all-the-icons-install-fonts t)))
 
-(pkg pos-tip)
-(pkg-github company-quickhelp "jack-faller/company-quickhelp"
-	:init
-	(add-hook 'company-tng-mode-hook 'company-quickhelp-mode)
-	(setq company-quickhelp-delay 0.1))
-
-;; (pkg company-box
-;; 	:defer t
-;; 	:after (company all-the-icons)
-;; 	:diminish company-box-mode
-;; 	:preface
-;; 	(add-hook 'company-tng-mode-hook 'company-box-mode)
-;; 	:config
-;; 	(setq company-box-doc-delay 0
-;; 				; company-box-doc-enable t
-;; 				company-box-doc-enable nil
-;; 				company-box-icons-all-the-icons
-;; 				`((Unknown . ,(all-the-icons-material "find_in_page" :height 0.8 :face 'all-the-icons-purple))
-;; 					(Text . ,(all-the-icons-material "text_fields" :height 0.8 :face 'all-the-icons-green))
-;; 					(Method . ,(all-the-icons-material "functions" :height 0.8 :face 'all-the-icons-red))
-;; 					(Function . ,(all-the-icons-material "functions" :height 0.8 :face 'all-the-icons-red))
-;; 					(Constructor . ,(all-the-icons-material "functions" :height 0.8 :face 'all-the-icons-red))
-;; 					(Field . ,(all-the-icons-material "functions" :height 0.8 :face 'all-the-icons-red))
-;; 					(Variable . ,(all-the-icons-material "adjust" :height 0.8 :face 'all-the-icons-blue))
-;; 					(Class . ,(all-the-icons-material "class" :height 0.8 :face 'all-the-icons-red))
-;; 					(Interface . ,(all-the-icons-material "settings_input_component" :height 0.8 :face 'all-the-icons-red))
-;; 					(Module . ,(all-the-icons-material "view_module" :height 0.8 :face 'all-the-icons-red))
-;; 					(Property . ,(all-the-icons-material "settings" :height 0.8 :face 'all-the-icons-red))
-;; 					(Unit . ,(all-the-icons-material "straighten" :height 0.8 :face 'all-the-icons-red))
-;; 					(Value . ,(all-the-icons-material "filter_1" :height 0.8 :face 'all-the-icons-red))
-;; 					(Enum . ,(all-the-icons-material "plus_one" :height 0.8 :face 'all-the-icons-red))
-;; 					(Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.8 :face 'all-the-icons-red))
-;; 					(Snippet . ,(all-the-icons-material "short_text" :height 0.8 :face 'all-the-icons-red))
-;; 					(Color . ,(all-the-icons-material "color_lens" :height 0.8 :face 'all-the-icons-red))
-;; 					(File . ,(all-the-icons-material "insert_drive_file" :height 0.8 :face 'all-the-icons-red))
-;; 					(Reference . ,(all-the-icons-material "collections_bookmark" :height 0.8 :face 'all-the-icons-red))
-;; 					(Folder . ,(all-the-icons-material "folder" :height 0.8 :face 'all-the-icons-red))
-;; 					(EnumMember . ,(all-the-icons-material "people" :height 0.8 :face 'all-the-icons-red))
-;; 					(Constant . ,(all-the-icons-material "pause_circle_filled" :height 0.8 :face 'all-the-icons-red))
-;; 					(Struct . ,(all-the-icons-material "streetview" :height 0.8 :face 'all-the-icons-red))
-;; 					(Event . ,(all-the-icons-material "event" :height 0.8 :face 'all-the-icons-red))
-;; 					(Operator . ,(all-the-icons-material "control_point" :height 0.8 :face 'all-the-icons-red))
-;; 					(TypeParameter . ,(all-the-icons-material "class" :height 0.8 :face 'all-the-icons-red))
-;; 					;; (Template   . ,(company-box-icons-image "Template.png"))))
-;; 					(Yasnippet . ,(all-the-icons-material "short_text" :height 0.8 :face 'all-the-icons-green))
-;; 					(ElispFunction . ,(all-the-icons-material "functions" :height 0.8 :face 'all-the-icons-red))
-;; 					(ElispVariable . ,(all-the-icons-material "check_circle" :height 0.8 :face 'all-the-icons-blue))
-;; 					(ElispFeature . ,(all-the-icons-material "stars" :height 0.8 :face 'all-the-icons-orange))
-;; 					(ElispFace . ,(all-the-icons-material "format_paint" :height 0.8 :face 'all-the-icons-pink)))
-;; 				company-box-icons-alist 'company-box-icons-all-the-icons))
+(pkg-github company-box "jack-faller/company-box"
+	:defer t
+	:after (company all-the-icons)
+	:diminish company-box-mode
+	:preface
+	(add-hook 'company-tng-mode-hook 'company-box-mode)
+	:config
+	(setq company-box-doc-delay 0.13
+				company-box-icons-all-the-icons
+				`((Unknown . ,(all-the-icons-material "find_in_page" :height 0.8 :face 'all-the-icons-purple))
+					(Text . ,(all-the-icons-material "text_fields" :height 0.8 :face 'all-the-icons-green))
+					(Method . ,(all-the-icons-material "functions" :height 0.8 :face 'all-the-icons-red))
+					(Function . ,(all-the-icons-material "functions" :height 0.8 :face 'all-the-icons-red))
+					(Constructor . ,(all-the-icons-material "functions" :height 0.8 :face 'all-the-icons-red))
+					(Field . ,(all-the-icons-material "functions" :height 0.8 :face 'all-the-icons-red))
+					(Variable . ,(all-the-icons-material "adjust" :height 0.8 :face 'all-the-icons-blue))
+					(Class . ,(all-the-icons-material "class" :height 0.8 :face 'all-the-icons-red))
+					(Interface . ,(all-the-icons-material "settings_input_component" :height 0.8 :face 'all-the-icons-red))
+					(Module . ,(all-the-icons-material "view_module" :height 0.8 :face 'all-the-icons-red))
+					(Property . ,(all-the-icons-material "settings" :height 0.8 :face 'all-the-icons-red))
+					(Unit . ,(all-the-icons-material "straighten" :height 0.8 :face 'all-the-icons-red))
+					(Value . ,(all-the-icons-material "filter_1" :height 0.8 :face 'all-the-icons-red))
+					(Enum . ,(all-the-icons-material "plus_one" :height 0.8 :face 'all-the-icons-red))
+					(Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.8 :face 'all-the-icons-red))
+					(Snippet . ,(all-the-icons-material "short_text" :height 0.8 :face 'all-the-icons-red))
+					(Color . ,(all-the-icons-material "color_lens" :height 0.8 :face 'all-the-icons-red))
+					(File . ,(all-the-icons-material "insert_drive_file" :height 0.8 :face 'all-the-icons-red))
+					(Reference . ,(all-the-icons-material "collections_bookmark" :height 0.8 :face 'all-the-icons-red))
+					(Folder . ,(all-the-icons-material "folder" :height 0.8 :face 'all-the-icons-red))
+					(EnumMember . ,(all-the-icons-material "people" :height 0.8 :face 'all-the-icons-red))
+					(Constant . ,(all-the-icons-material "pause_circle_filled" :height 0.8 :face 'all-the-icons-red))
+					(Struct . ,(all-the-icons-material "streetview" :height 0.8 :face 'all-the-icons-red))
+					(Event . ,(all-the-icons-material "event" :height 0.8 :face 'all-the-icons-red))
+					(Operator . ,(all-the-icons-material "control_point" :height 0.8 :face 'all-the-icons-red))
+					(TypeParameter . ,(all-the-icons-material "class" :height 0.8 :face 'all-the-icons-red))
+					;; (Template   . ,(company-box-icons-image "Template.png"))))
+					(Yasnippet . ,(all-the-icons-material "short_text" :height 0.8 :face 'all-the-icons-green))
+					(ElispFunction . ,(all-the-icons-material "functions" :height 0.8 :face 'all-the-icons-red))
+					(ElispVariable . ,(all-the-icons-material "check_circle" :height 0.8 :face 'all-the-icons-blue))
+					(ElispFeature . ,(all-the-icons-material "stars" :height 0.8 :face 'all-the-icons-orange))
+					(ElispFace . ,(all-the-icons-material "format_paint" :height 0.8 :face 'all-the-icons-pink)))
+				company-box-icons-alist 'company-box-icons-all-the-icons))
 
 (pkg company-prescient
 	:after (company prescient)
