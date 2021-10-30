10765169de221de1f295d1f680786430b3750210
Jack Faller <jack.t.faller@gmail.com>
2021-10-30 09:36:14 +0100

    Fixed gc threshold being reset at startup

--- early-init.el
+++ early-init.el
@@ -1,7 +1,5 @@
 (setq package-enable-at-startup nil)
 
-;; disable gc during startup
-(setq startup/gc-cons-threshold gc-cons-threshold)
+;; disable gc during startup and set to a higher number after
 (setq gc-cons-threshold most-positive-fixnum)
-(defun startup/reset-gc () (setq gc-cons-threshold startup/gc-cons-threshold))
-(add-hook 'emacs-startup-hook 'startup/reset-gc)
+(add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold 100000000)))
